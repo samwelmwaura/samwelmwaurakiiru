b9d31e7ab99870523633dd1c51e635ab6a217bfd
samwelmwaura <samwelmkiiru@gmail.com>
2024-11-27 06:25:48 +0300

    commit first

---
+++ main.js
@@ -0,0 +1,10 @@
+const videoBtn = document.querySelector(".guka-live-coverage .btn");
+const eulogyBtn = document.querySelector(".guka-eulogy .btn");
+
+videoBtn.addEventListener("click", (e) => {
+    alert("Starting Soon")
+})
+
+eulogyBtn.addEventListener("click", (e) => {
+    alert("Being Uploaded Soon")
+})
